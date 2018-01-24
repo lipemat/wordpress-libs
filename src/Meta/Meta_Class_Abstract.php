64cd5a6e7233ea03ca55015d4f3fa49da351eedb
lipemat <mat@matlipe.com>
2018-01-24 13:10:21 -0500

    cleanup meta class abstract

--- src/Meta/Meta_Class_Abstract.php
+++ src/Meta/Meta_Class_Abstract.php
@@ -12,18 +12,33 @@ namespace Lipe\Lib\Meta;
  */
 abstract class Meta_Class_Abstract {
 
-	abstract public function get_keys();
+	/**
+	 * All meta keys used by this class must be returned here.
+	 * Used to map keys back to the proper class when retrieving meta values
+	 *
+	 * @return array
+	 */
+	abstract public function get_keys() : array;
 
 
 	/**
+	 * Should be called via the Meta_Repo
+	 *
+	 * Not recommended to call directly via the call which extends this
+	 * abstract, although technically it will work.
+	 *
 	 *
 	 * @param int    $object_id
 	 * @param string $key
-	 * @param string $meta_type - user, term, post (defaults to 'post')
+	 * @param string $meta_type - user, term, post
+	 *
+	 * @see Meta_Repo::get_meta()
+	 *
+	 * @internal
 	 *
 	 * @return mixed
 	 */
-	abstract public function get_meta( $object_id, string $key, string $meta_type = 'post' );
+	abstract public function get_meta( $object_id, string $key, string $meta_type );
 
 
 	public function __construct() {
@@ -34,20 +49,4 @@ abstract class Meta_Class_Abstract {
 	protected function register_with_repo() : void {
 		Meta_Repo::instance()->register_keys( $this, $this->get_keys() );
 	}
-
-
-	/**
-	 *
-	 * @param int    $object_id
-	 * @param string $key
-	 * @param string $meta_type - user, term, post (defaults to 'post')
-	 *
-	 * @static
-	 *
-	 * @return mixed
-	 */
-	public static function meta( $object_id, string $key, string $meta_type = 'post' ) {
-		return static::instance()->get_meta( $object_id, $key, $meta_type );
-	}
-
 }
