9d91f695230faad888f0a895cc8585e319dd8fa9
lipemat <mat@matlipe.com>
2018-04-30 13:56:37 -0400

    add Rest Api filter param avaialble support

---
+++ src/Rest_Api/Filter_Param.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Lipe\Lib\Rest_Api;
+
+use Lipe\Lib\Traits\Singleton;
+
+/**
+ * Allows using any public wp_query argument to be added to the url
+ * via a filter[<var>]=<value> param
+ *
+ * @uses    Run init() on this class before the rest_api_init hook fires.
+ * @example /wp-json/wp/v2/posts?filter[cat]=4
+ *
+ * @author  Mat Lipe
+ * @since   1.7.0
+ *
+ * @package Lipe\Project\Rest_Api
+ */
+class Filter_Param {
+	use Singleton;
+
+
+	protected function hook() : void {
+		add_action( 'rest_api_init', [ $this, 'add_filters' ] );
+	}
+
+
+	/**
+	 * Add the necessary filter to each post type
+	 **/
+	public function add_filters() : void {
+		foreach ( get_post_types( [ 'show_in_rest' => true ], 'objects' ) as $post_type ) {
+			add_filter( 'rest_' . $post_type->name . '_query', [ $this, 'add_filter_param' ], 10, 2 );
+		}
+	}
+
+
+	/**
+	 * Add the filter parameter
+	 *
+	 * @param  array            $args    The query arguments.
+	 * @param  \WP_REST_Request $request Full details about the request.
+	 *
+	 * @return array $args.
+	 **/
+	public function add_filter_param( $args, $request ) : array {
+		if ( empty( $request['filter'] ) || ! \is_array( $request['filter'] ) ) {
+			return $args;
+		}
+
+		$filter = $request['filter'];
+
+		if ( isset( $filter['posts_per_page'] ) && ( (int) $filter['posts_per_page'] >= 1 && (int) $filter['posts_per_page'] <= 100 ) ) {
+			$args['posts_per_page'] = $filter['posts_per_page'];
+		}
+
+		global $wp;
+		$vars = apply_filters( 'query_vars', $wp->public_query_vars );
+
+		foreach ( $vars as $var ) {
+			if ( isset( $filter[ $var ] ) ) {
+				$args[ $var ] = $filter[ $var ];
+			}
+		}
+
+		return $args;
+	}
+}
