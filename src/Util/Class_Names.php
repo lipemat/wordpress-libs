a93c5798c4edb40deae052d2001d285413711c37
lipemat <mat@matlipe.com>
2018-10-19 15:08:38 -0400

    Support adding/removing classes from Class_Names after construction

--- src/Util/Class_Names.php
+++ src/Util/Class_Names.php
@@ -5,18 +5,24 @@ namespace Lipe\Lib\Util;
 /**
  * Conditionally add css classes to an element
  *
- * Mirrored after npm classnames
+ * Mirrored after npm classnames and enhanced
  * @link https://www.npmjs.com/package/classnames
  *
  * @example $class = new Class_Names( [ 'top' => false, 'bottom' => true ] ); //echo $class; outputs "bottom"
+ *
  * @example $class = new Class_Names( [ 'top', 'bottom' ] ); //"echo $class; outputs top bottom"
+ *
+ * @example $class  = new Class_Names( [ $styles['tab'] ] );
+ *          //conditionally add an active class as we go
+ *          $class[ 'active' ] = isset( $_POST['domain_list'] );
+ *
  *
  * @author  Mat Lipe
  * @since   1.4.0
  *
  * @package Lipe\Lib\Util
  */
-class Class_Names {
+class Class_Names implements \ArrayAccess {
 
 	private $classes = [];
 
@@ -38,4 +44,28 @@ class Class_Names {
 		return implode( ' ', $this->classes );
 	}
 
+
+	public function offsetExists( $class ) : bool {
+		return isset( $this->classes[ $class ] );
+	}
+
+
+	public function offsetGet( $class ) {
+		return $this->classes[ $class ];
+	}
+
+
+	public function offsetSet( $class, $active ) : void {
+		if ( (bool) $active ) {
+			$this->classes[] = $class;
+		} else {
+			unset( $this->classes[ $class ] );
+		}
+	}
+
+
+	public function offsetUnset( $class ) : void {
+		unset( $this->classes[ $class ] );
+	}
 }
+
