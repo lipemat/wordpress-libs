c36521add80e956534f660f179a49b76bf401d88
lipemat <mat@matlipe.com>
2019-06-28 15:18:34 -0400

    Cleanup the Version trait a bit.

--- src/Traits/Version.php
+++ src/Traits/Version.php
@@ -4,7 +4,9 @@ namespace Lipe\Lib\Traits;
 
 /**
  * Allow running things specific to a class one time
- * based on it's internal version.
+ * based on it's internal version. Each class get's it's
+ * own unique identifier and all uses of `run_for_version` will
+ * use the same identifier and previous version.
  *
  * For global version tracking
  *
@@ -15,30 +17,52 @@ namespace Lipe\Lib\Traits;
  *
  */
 trait Version {
-	private $option = 'lipe/lib/traits/versions';
+	private $_option = 'lipe/lib/traits/versions';
+
+	private $_version = '0.0.1';
 
 
-	protected function run_for_version( callable $func, float $version, ...$extra ) : void {
-		if ( $this->update_required( $version ) ) {
-			$func( ...$extra );
+	protected function run_for_version( callable $func, string $version, ...$extra ) {
+		$this->_version = $version;
+		if ( $this->update_required() ) {
+			$this->update_version();
 
-			$this->update_version( $version );
+			return $func( ...$extra );
 		}
 	}
 
 
-	private function update_version( float $version ) : void {
-		$versions                               = get_option( $this->option, [] );
-		$versions[ \get_parent_class( $this ) ] = $version;
+	private function update_version() : void {
+		$versions                                    = $this->get_versions();
+		$versions[ $this->get_version_identifier() ] = $this->_version;
 
-		update_option( $this->option, $versions );
+		update_option( $this->_option, $versions );
 	}
 
 
-	private function update_required( float $version ) : bool {
-		$versions = get_option( $this->option, [] );
+	private function update_required() : bool {
+		$versions = $this->get_versions();
 
-		return empty( $versions[ \get_parent_class( $this ) ] ) || version_compare( $versions[ \get_parent_class( $this ) ], $version, '<' );
+		return empty( $versions[ $this->get_version_identifier() ] ) || version_compare( $versions[ $this->get_version_identifier() ], $this->_version, '<' );
 	}
 
+
+	private function get_versions() : array {
+		return get_option( $this->_option, [] );
+	}
+
+
+	/**
+	 * @notice Anonymous classes do not have identifiers and may not be used.
+	 *
+	 * @throws \ReflectionException
+	 * @return string
+	 */
+	private function get_version_identifier() : string {
+		if ( ( new \ReflectionClass( $this ) )->isAnonymous() ) {
+			throw new \BadMethodCallException( 'You may not use the Version Trait with anonymous classes, you will have to implement what you need within your anonymous class.' );
+		}
+
+		return get_class( $this );
+	}
 }
