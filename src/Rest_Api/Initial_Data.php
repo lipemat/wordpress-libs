ff732ec91fbdd208d041ef8854482d7899114185
lipemat <mat@matlipe.com>
2020-08-21 14:44:55 -0600

    Support excluding '_links' from Initial_Data from REST
    Introduce a new parameter to selectively include the '_links' in
    returned data.
    
    @Breaking Changes
    Move the `embed` parameter to last argument in method signatures which
    is used least of all.
    If you are actually using this paramater (uncommon) you'll need to
    update your calls.

--- src/Rest_Api/Initial_Data.php
+++ src/Rest_Api/Initial_Data.php
@@ -11,32 +11,30 @@ use Lipe\Lib\Traits\Singleton;
  * Use most commonly to get the json data without making a request to the api
  * Thus preventing an anti-pattern when using React etc.
  *
- * @since June, 2017
- * @since 2.12.0 (greatly revamped and added comment support)
- *
  */
 class Initial_Data {
 	use Singleton;
 	use Memoize;
-
 
 	/**
 	 * Turn an array of Comments into there matching data format
 	 * provided by the JSON API Server.
 	 *
 	 * @param \WP_Comment[] $comments
-	 * @param bool          $embed_links - To embed the links inside the response (default to false);
+	 * @param bool          $with_links - To include links inside the response.
+	 * @param bool          $embed      - To embed the links inside the response.
 	 *
 	 * @since 2.12.0
+	 * @since 2.19.0 - Support excluding the entire '_links' data.
 	 *
 	 * @return array
 	 *
 	 */
-	public function get_comments_data( array $comments, bool $embed_links = false ) : array {
+	public function get_comments_data( array $comments, bool $with_links = false, bool $embed = false ) : array {
 		$controller = new \WP_REST_Comments_Controller();
 
-		return array_map( function ( $comment ) use ( $controller, $embed_links ) {
-			return $this->get_response( $controller, $comment, $embed_links );
+		return array_map( function ( $comment ) use ( $controller, $with_links, $embed ) {
+			return $this->get_response( $controller, $comment, $with_links, $embed );
 		}, $comments );
 	}
 
@@ -45,50 +43,65 @@ class Initial_Data {
 	 * Gets global posts data from the JSON API server
 	 * Alternatively an array of posts may be passed to be converted to json data
 	 *
-	 * @param \WP_Post[]|null $posts       - Array of post objects
-	 *                                     (defaults to global WP_Query->posts)
+	 * @param \WP_Post[]|null $posts      - Array of post objects (defaults to global WP_Query->posts)
+	 * @param bool            $with_links - To include links inside the response.
+	 * @param bool            $embed      - To embed the links inside the response.
 	 *
-	 * @param bool            $embed_links - To embed the links inside the response (default to true);
+	 * @since 2.19.0 - Support excluding the entire '_links' data.
 	 *
 	 * @return array
 	 */
-	public function get_post_data( ?array $posts = null, bool $embed_links = true ) : array {
+	public function get_post_data( ?array $posts = null, bool $with_links = false, bool $embed = false ) : array {
 		if ( null === $posts && ! is_404() ) {
 			$posts = $GLOBALS['wp_query']->posts;
 		}
 
-		return array_map( function ( $post ) use ( $embed_links ) {
+		return array_map( function ( $post ) use ( $with_links, $embed ) {
 			$controller = new \WP_REST_Posts_Controller( $post->post_type );
 
-			return $this->get_response( $controller, $post, $embed_links );
+			return $this->get_response( $controller, $post, $with_links, $embed );
 		}, $posts );
 	}
 
 
 	/**
+	 * Mimic response from the REST server for the provided controller.
+	 *
 	 * @param \WP_REST_Controller  $controller
 	 * @param \WP_Post|\WP_Comment $item
-	 * @param bool                 $embed_links
+	 * @param bool                 $with_links - To include links inside the response.
+	 * @param bool                 $embed      - To embed the links inside the response.
 	 *
 	 * @link  https://developer.wordpress.org/rest-api/using-the-rest-api/linking-and-embedding/#embedding
 	 *
 	 * @since 2.12.0
+	 * @since 2.19.0 - Support excluding the entire '_links' data.
 	 *
 	 * @return array
 	 *
 	 */
-	protected function get_response( \WP_REST_Controller $controller, $item, bool $embed_links = true ) : array {
-		return $this->get_server()->response_to_data( $controller->prepare_item_for_response( $item, $this->get_request() ), $embed_links );
+	protected function get_response( \WP_REST_Controller $controller, $item, bool $with_links = false, bool $embed = false ) : array {
+		$data = rest_get_server()->response_to_data(
+			$controller->prepare_item_for_response( $item, $this->get_request() ),
+			$embed
+		);
+		if ( ! $with_links ) {
+			unset( $data['_links'] );
+		}
+		return $data;
 	}
 
 
 	/**
+	 * Get an instance of the WP_REST_Request setup with
+	 * the 'view' context.
+	 *
 	 * @since 2.12.0
 	 * @return \WP_REST_Request
 	 */
 	protected function get_request() : \WP_REST_Request {
 		return $this->once( function () {
-			$request            = new \WP_REST_Request();
+			$request = new \WP_REST_Request();
 			$request['context'] = 'view';
 
 			return $request;
@@ -97,17 +110,10 @@ class Initial_Data {
 
 
 	/**
-	 * @since 2.12.0
-	 * @return \WP_REST_Server
+	 * @deprecated 2.19.0
 	 */
 	protected function get_server() : \WP_REST_Server {
-		global $wp_rest_server;
-		if ( null === $wp_rest_server ) {
-			$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
-			$wp_rest_server       = new $wp_rest_server_class();
-			do_action( 'rest_api_init' );
-		}
-
-		return $wp_rest_server;
+		\_deprecated_function( __METHOD__, '2.19.0', '\rest_get_server' );
+		return rest_get_server();
 	}
 }
