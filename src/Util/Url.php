9171b6f871a7656e71bebbf3ef3a2f600927aa0f
lipemat <mat@matlipe.com>
2020-11-29 10:57:45 -0700

    Introduce `get_url_arg` to `Url` class
    Simple retrieval of a value from a URL string by key.
    
    Mirrors the `add_query_arg` WP function.

--- src/Util/Url.php
+++ src/Util/Url.php
@@ -26,4 +26,21 @@ class Url {
 
 		return $prefix .$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; //phpcs:ignore
 	}
+
+
+	/**
+	 * Retrieve the value of a query argument from a URL string.
+	 *
+	 * @param string $url - URL to retrieve from.
+	 * @param string $key - Name of URL key to retrieve.
+	 *
+	 * @since 2.20.0
+	 *
+	 * @return string|array|null
+	 */
+	public function get_query_arg( string $url, string $key ) {
+		$query_str = wp_parse_url( $url, PHP_URL_QUERY );
+		wp_parse_str( $query_str, $query_vars );
+		return $query_vars[ $key ] ?? null;
+	}
 }
