33c27480873f18be57fa2ca32944df638df55cc9
lipemat <mat@matlipe.com>
2022-01-04 11:10:03 -0500

    Automatically add menu item properties via `Menu_Item_Trait::get_object`
    If a `WP_Post` is passed from outside `wp_get_nav_menu_items` or if
    and id is passed, automatically add the expected extra properties
    to the `WP_Post` object the first time `get_object` is called.

--- src/Menu/Menu_Item_Trait.php
+++ src/Menu/Menu_Item_Trait.php
@@ -10,6 +10,8 @@ use Lipe\Lib\Post_Type\Post_Object_Trait;
  * Same as other post types except additional properties are added
  * by `wp_setup_nav_menu_item`.
  *
+ * @since 3.7.0
+ *
  * @property int    $db_id
  * @property int    $menu_item_parent
  * @property int    $object_id
@@ -27,4 +29,43 @@ use Lipe\Lib\Post_Type\Post_Object_Trait;
  */
 trait Menu_Item_Trait {
 	use Post_Object_Trait;
+
+	/**
+	 * Retrieve the post then run it through `wp_setup_nav_menu_item`
+	 * to add additional properties.
+	 *
+	 * Also, automatically adds properties to a `WP_Post` passed to `factory`
+	 * without coming from `wp_get_nav_menu_items`.
+	 *
+	 * @return \WP_Post|null
+	 */
+	public function get_object() : ?\WP_Post {
+		if ( null === $this->post ) {
+			$this->post = get_post( $this->post_id );
+		}
+		if ( ! empty( $this->post ) && ! property_exists( $this->post, 'db_id' ) ) {
+			$this->post = wp_setup_nav_menu_item( $this->post );
+		}
+
+		return $this->post;
+	}
+
+
+	/**
+	 * Pass either an ID of the menu item, or a WP_Post
+	 * returned from `wp_get_nav_menu_items`.
+	 *
+	 * @param int|\WP_Post $post - ID or post object, null is not supported.
+	 *
+	 * @static
+	 *
+	 * @return static
+	 */
+	public static function factory( $post ) {
+		if ( empty( $post ) ) {
+			_doing_it_wrong( __METHOD__, 'Null is not supported for menu items.', '3.7.1' );
+		}
+		return new static( $post );
+	}
+
 }
