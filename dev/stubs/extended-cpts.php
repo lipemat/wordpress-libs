c37bb46827bd545620e8d6d15de1ecb85e19f782
lipemat <mat@matlipe.com>
2022-06-11 08:25:29 -0400

    Use stub for extended-cpts instead of a require
    Not all projects will have extended-cpts available. For PHPSTan to
    pass on those projects we use a stub.

---
+++ dev/stubs/extended-cpts.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace {
+
+	function register_extended_post_type( string $post_type, array $args = [], array $names = [] ) : Extended_CPT {
+	}
+
+	function register_extended_taxonomy( string $taxonomy, $object_type, array $args = [], array $names = [] ) : Extended_Taxonomy {
+	}
+}
