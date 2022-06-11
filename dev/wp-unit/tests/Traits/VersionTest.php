6597cc163e5d1a1c431cdf123e3b29e76cdb6de3
lipemat <mat@matlipe.com>
2022-06-11 08:55:00 -0400

    Fix Verstest for external projects

--- dev/wp-unit/tests/Traits/VersionTest.php
+++ dev/wp-unit/tests/Traits/VersionTest.php
@@ -1,18 +1,19 @@
 <?php
+namespace Lipe\Lib\Traits;
+
 /**
  * @author Mat Lipe
  * @since  June, 2019
  *
  */
-
-namespace Lipe\Lib\Traits;
-
-use Lipe\Project\User\Roles\Limited_Editor;
+class VT_ {
+	use Version;
+}
 
 class VersionTest extends \WP_UnitTestCase {
 
 	public function test_run_for_version() : void {
-		$t = new Limited_Editor();
+		$t = new VT_();
 		\call_private_method( $t, 'run_for_version', [
 			function( $o, $t, $th ) {
 				$this->assertEquals( 'one', $o );
@@ -29,5 +30,12 @@ class VersionTest extends \WP_UnitTestCase {
 		$this->assertNull( $outside );
 		$outside = \call_private_method( $t, 'run_for_version', [ '__return_true', '1.0.1' ] );
 		$this->assertTrue( $outside );
+
+		$this->expectExceptionMessage( 'You may not use the Version Trait with anonymous classes, you will have to implement what you need within your anonymous class.' );
+		$this->expectException( 'BadMethodCallException' );
+		$f = new class() {
+			use Version;
+		};
+		\call_private_method( $f, 'run_for_version', [ '__return_true', '1.0.0' ] );
 	}
 }
