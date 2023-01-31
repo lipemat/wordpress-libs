87d2db592b03b39696f2889a59226350a79f2042
lipemat <mat@matlipe.com>
2023-01-31 17:12:00 -0500

    Revert "Introduce Url::get_blog_pagenum_link"
    Not universal enough to include in this library. Need more
    use cases before introducing.
    
    This reverts commit fa31280158a375227a522476541c211e0060d348.

--- src/Util/Url.php
+++ src/Util/Url.php
@@ -35,42 +35,4 @@ class Url {
 		wp_parse_str( $query_str, $query_vars );
 		return $query_vars[ $key ] ?? null;
 	}
-
-
-	/**
-	 * Get the link to the blog page with the page number
-	 * added to it from anywhere on the site.
-	 *
-	 * Similar to `get_pagenum_link` expect it works when you
-	 * are not already on the blog page.
-	 *
-	 * Falls back to `get_pagenum_link` if no blog page is set
-	 * in Reading options.
-	 *
-	 * @since 3.15.0
-	 *
-	 * @param int $pagenum - Page number for the link.
-	 *
-	 * @return string
-	 */
-	public function get_blog_pagenum_link( int $pagenum = 1 ) : string {
-		global $wp_rewrite;
-
-		$blog_page = get_option( 'page_for_posts' );
-		$base = get_permalink( $blog_page );
-		if ( empty( $blog_page ) || empty( $base ) ) {
-			return get_pagenum_link( $pagenum );
-		}
-
-		if ( $pagenum < 2 ) {
-			return $base;
-		}
-
-		if ( ! $wp_rewrite->using_permalinks() || is_admin() ) {
-			return add_query_arg( [ 'paged' => $pagenum ], $base );
-		}
-
-		return trailingslashit( $base ) . user_trailingslashit( $wp_rewrite->pagination_base . '/' . $pagenum, 'paged' );
-	}
-
 }
