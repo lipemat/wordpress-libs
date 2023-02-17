bdc760b4be7278b615ae43f67af9393ad6969604
lipemat <mat@matlipe.com>
2023-02-17 13:00:38 -0500

    Set global memcached_servers to enable external object cache
    When available, the tests must be able to access the Memcached
    server to complete correctly.
    
    Does not apply if not using a Memcached based object cache.

--- dev/wp-unit/default-local-config.php
+++ dev/wp-unit/default-local-config.php
@@ -4,14 +4,16 @@
  *
  * Loaded if no local-config.php is provided.
  */
+
+// Point to local memcache servers (Requirement of hosts like WPE).
+$GLOBALS['memcached_servers'] = [ '127.0.0.1:11211' ];
+
 define( 'DB_NAME', 'starting-point' );
 define( 'DB_USER', 'wplibs' );
 define( 'DB_PASSWORD', 'hle1.qRA8W[BnR03' );
 define( 'DB_HOST', 'localhost' );
 define( 'BOOTSTRAP', 'E:/SVN/wp-unit/includes/bootstrap.php' );
 define( 'WP_UNIT_DIR', 'E:/SVN/wp-unit' );
-
-// Root of your site
 define( 'WP_TESTS_DOMAIN', 'wp-libs.loc' );
 define( 'WP_TESTS_EMAIL', 'unit-tests@test.com' );
 define( 'WP_TESTS_TITLE', 'WP Libs unit tests' );
