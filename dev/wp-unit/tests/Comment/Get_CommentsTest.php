2e4b70b8a8e5851451b4901020d014c82c0fa51d
lipemat <mat@matlipe.com>
2023-03-30 14:41:59 -0400

    Create unit test for `Get_Comments::merge_query`

--- dev/wp-unit/tests/Comment/Get_CommentsTest.php
+++ dev/wp-unit/tests/Comment/Get_CommentsTest.php
@@ -57,4 +57,22 @@ class Get_CommentsTest extends \WP_UnitTestCase {
 			'comment__in' => [ 4 ],
 		], $args->get_args() );
 	}
+
+
+	public function test_merge_query() : void {
+		$query = new \WP_Comment_Query( [
+			'parent' => '4455',
+		] );
+		$previous = $query->query_vars;
+
+		$args = new Get_Comments();
+		$args->karma = 4;
+		$args->orderby( 'user_id' );
+		$args->merge_query( $query );
+
+		$previous['orderby'] = 'user_id';
+		$previous['karma'] = 4;
+		$previous['parent'] = 4455;
+		$this->assertEquals( $previous, $query->query_vars );
+	}
 }
