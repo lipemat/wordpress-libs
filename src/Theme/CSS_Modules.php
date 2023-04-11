a8ebbf3105dc08aaf72f9dafa5e6c93ec1cb203d
lipemat <mat@matlipe.com>
2023-04-11 15:56:48 -0400

    Introduce support for combined CSS_Module JSON files
    Opt in support to read all CSS classes from a single file instead
    of different files per module.
    
    Improves performance because we only need to read 1 file.
    
    Leave original implementation in place for backward compability (for now).

--- src/Theme/CSS_Modules.php
+++ src/Theme/CSS_Modules.php
@@ -1,7 +1,10 @@
 <?php
 
+declare( strict_types=1 );
+
 namespace Lipe\Lib\Theme;
 
+use Lipe\Lib\Traits\Memoize;
 use Lipe\Lib\Traits\Singleton;
 
 /**
@@ -12,26 +15,116 @@ use Lipe\Lib\Traits\Singleton;
  *
  */
 class CSS_Modules {
+	use Memoize;
 	use Singleton;
 
 	/**
-	 * path
+	 * Root path of the CSS module JSON files.
 	 *
 	 * @var string
 	 */
-	protected $path;
+	protected string $path;
+
+	/**
+	 * Prepend a string to the path of the file when retrieving
+	 * the CSS classnames.
+	 *
+	 * Uses to shorten the passed file names if they share a common directory.
+	 *
+	 * @example 'template-parts'
+	 *
+	 * @var string
+	 */
+	protected string $prepend = '';
+
+	/**
+	 * Use the `combined.json` file instead of separate .json files
+	 * per each module.
+	 *
+	 * @var bool
+	 */
+	protected bool $combined = false;
+
+	/**
+	 * Name of JSON file when using a combined JSON file.
+	 *
+	 * @default 'css-modules.json'
+	 *
+	 * @var string
+	 */
+	protected string $combined_filename = 'css-modules.json';
 
 
-	public function set_path( string $path ) : void {
+	/**
+	 * Set the path to the CSS module JSON files with an
+	 * optional file prepend.
+	 *
+	 *
+	 * @param string $path         - Path to CSS modules JSON file(s).
+	 * @param string $file_prepend - Prepend a path to the $file during lookup.
+	 *
+	 * @return void
+	 */
+	public function set_path( string $path, string $file_prepend = '' ) : void {
 		$this->path = $path;
+		$this->prepend = $file_prepend;
 	}
 
 
-	public function styles( string $file ) : array {
-		$json = file_get_contents( trailingslashit( $this->path ) . "{$file}.pcss.json" ); //phpcs:ignore
-		$classes = json_decode( $json, true );
+	/**
+	 * Do we want to use the `combined.json` file?
+	 *
+	 * @param bool        $combined - Use combined file or not.
+	 * @param string|null $filename - Name of the combined JSON file.
+	 *
+	 * @return void
+	 */
+	public function set_combined( bool $combined, ?string $filename = null ) : void {
+		$this->combined = $combined;
+		if ( $filename ) {
+			$this->combined_filename = $filename;
+		}
+	}
 
+
+	/**
+	 * CSS class names for a single CSS module.
+	 *
+	 * @param string $file - File with the path from CSS module JSON directory.
+	 *
+	 * @return array
+	 */
+	public function styles( string $file ) : array {
+		$file = $this->prepend . $file . '.pcss';
+		if ( $this->combined ) {
+			return $this->get_combined_css_classes()[ $file ] ?? [];
+		}
+
+		try {
+			//phpcs:ignore -- Reading local file.
+			$classes = json_decode( file_get_contents( trailingslashit( $this->path ) . "{$file}.json" ), true, 3, JSON_THROW_ON_ERROR );
+		} catch ( \JsonException $exception ) {
+			return [];
+		}
 		return (array) $classes;
+	}
+
+
+	/**
+	 * Retrieve the CSS module classes from the `combined.json` file.
+	 *
+	 * @return array
+	 */
+	protected function get_combined_css_classes() : array {
+		return $this->once( function() {
+			try {
+				//phpcs:ignore -- Reading local file.
+				$classes = \json_decode( \file_get_contents( trailingslashit( $this->path ) . $this->combined_filename ), true, 3, JSON_THROW_ON_ERROR );
+			} catch ( \JsonException $exception ) {
+				return [];
+			}
+			return (array) $classes;
+		}, __METHOD__ );
 	}
 
 }
