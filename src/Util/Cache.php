7be817474468f68e9b80e48144c46ab573c5d3cb
lipemat <mat@matlipe.com>
2023-10-11 19:22:27 -0500

    Add phpdoc for return of cache::get

--- src/Util/Cache.php
+++ src/Util/Cache.php
@@ -27,7 +27,7 @@ class Cache {
 	 *
 	 * @return void
 	 */
-	public function hook() : void {
+	public function hook(): void {
 		add_action( 'init', [ $this, 'maybe_clear_cache' ], 9, 0 );
 
 		if ( current_user_can( 'manage_options' ) ) {
@@ -48,7 +48,7 @@ class Cache {
 	 *
 	 * @return bool
 	 */
-	public function set( $key, $value, string $group = self::DEFAULT_GROUP, int $expire_in_seconds = 0 ) : bool {
+	public function set( $key, $value, string $group = self::DEFAULT_GROUP, int $expire_in_seconds = 0 ): bool {
 		$group = $this->get_group_key( $group );
 
 		if ( null === $value ) {
@@ -68,7 +68,7 @@ class Cache {
 	 * @param mixed             $key   - Cache key. Array, string, or object.
 	 * @param string            $group - The group to store the cache in.
 	 *
-	 * @return false|mixed
+	 * @return false|mixed - false on failure to retrieve contents, or the cache contents on success.
 	 */
 	public function get( $key, string $group = self::DEFAULT_GROUP ) {
 		$group = $this->get_group_key( $group );
@@ -89,7 +89,7 @@ class Cache {
 	 *
 	 * @return bool
 	 */
-	public function delete( $key, string $group = self::DEFAULT_GROUP ) : bool {
+	public function delete( $key, string $group = self::DEFAULT_GROUP ): bool {
 		$group = $this->get_group_key( $group );
 
 		return wp_cache_delete( $this->filter_key( $key ), $group );
@@ -105,7 +105,7 @@ class Cache {
 	 *
 	 * @return void
 	 */
-	public function flush_group( string $group = self::DEFAULT_GROUP ) : void {
+	public function flush_group( string $group = self::DEFAULT_GROUP ): void {
 		wp_cache_set( 'last_changed', microtime(), $group );
 	}
 
@@ -115,8 +115,8 @@ class Cache {
 	 *
 	 * @return void
 	 */
-	public function maybe_clear_cache() : void {
-		if ( empty( $_REQUEST[ static::QUERY_ARG ] ) || empty( $_REQUEST['_wpnonce'] ) || ! wp_verify_nonce( $_REQUEST['_wpnonce'], static::QUERY_ARG ) ) {
+	public function maybe_clear_cache(): void {
+		if ( empty( $_REQUEST[ static::QUERY_ARG ] ) || empty( $_REQUEST['_wpnonce'] ) || ! wp_verify_nonce( sanitize_text_field( \wp_unslash( $_REQUEST['_wpnonce'] ) ), static::QUERY_ARG ) ) {
 			return;
 		}
 		wp_cache_flush();
@@ -134,7 +134,7 @@ class Cache {
 	 *
 	 * @return void
 	 */
-	public function add_admin_bar_button( \WP_Admin_Bar $admin_bar ) : void {
+	public function add_admin_bar_button( \WP_Admin_Bar $admin_bar ): void {
 		$admin_bar->add_menu( [
 			'parent' => '',
 			'id'     => static::QUERY_ARG,
@@ -153,7 +153,7 @@ class Cache {
 	 *
 	 * @return string
 	 */
-	protected function get_group_key( string $group ) : string {
+	protected function get_group_key( string $group ): string {
 		return $group . ':' . wp_cache_get_last_changed( $group );
 	}
 
@@ -178,5 +178,4 @@ class Cache {
 
 		return $key;
 	}
-
 }
