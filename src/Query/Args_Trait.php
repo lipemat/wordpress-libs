90f9fa315dbceab0fedd5963a8f352a16a96122e
lipemat <mat@matlipe.com>
2024-02-20 15:50:16 -0600

    Support sub args classes when flattening from the Args_Trait

--- src/Query/Args_Trait.php
+++ src/Query/Args_Trait.php
@@ -22,6 +22,15 @@ trait Args_Trait {
 	 */
 	protected array $clauses = [];
 
+	/**
+	 * Subclasses which contain their own args to be flattened via `get_args`.
+	 *
+	 * The array key is the property name, and the value is the class to be flattened.
+	 *
+	 * @var array<string, Args_Interface>
+	 */
+	protected array $sub_args = [];
+
 
 	/**
 	 * Optionally pass existing arguments to preload this class.
@@ -51,10 +60,13 @@ trait Args_Trait {
 		foreach ( $this->clauses as $clause ) {
 			$clause->flatten( $this );
 		}
+		foreach ( $this->sub_args as $prop => $sub_class ) {
+			$this->{$prop} = $sub_class->get_args();
+		}
 
 		$args = [];
 		foreach ( get_object_vars( $this ) as $_var => $_value ) {
-			if ( ! isset( $this->{$_var} ) || 'clauses' === $_var ) {
+			if ( ! isset( $this->{$_var} ) || 'clauses' === $_var || 'sub_args' === $_var ) {
 				continue;
 			}
 			$args[ $_var ] = $this->{$_var};
