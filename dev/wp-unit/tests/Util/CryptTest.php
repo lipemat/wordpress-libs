49de91167573addafd084b6a529666415f4fc66c
lipemat <mat@matlipe.com>
2024-05-23 13:27:22 -0400

    Introduce `\Lipe\Lib\Util\Crypt::is_encrypted` method
    - Check if a string is properly encrypted.
    - Does not validate the encryption key.

--- dev/wp-unit/tests/Util/CryptTest.php
+++ dev/wp-unit/tests/Util/CryptTest.php
@@ -6,7 +6,7 @@ use Lipe\Lib\Util\Crypt;
 
 class CryptTest extends \WP_UnitTestCase {
 
-	public function test_encrypt() : void {
+	public function test_encrypt(): void {
 		$crypt = Crypt::factory( 'in AppContainer (created by HotExportedApp)' .
 		                         'in HotExportedApp (created by ForwardRef)' .
 		                         'in AppContainer (created by ForwardRef)' .
@@ -22,5 +22,30 @@ class CryptTest extends \WP_UnitTestCase {
 		$this->assertMatchesRegularExpression( '/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $encrypted );
 
 		$this->assertEquals( 'Hello hi how are you? :)', $crypt->decrypt( $encrypted ) );
+	}
+
+
+	/**
+	 * @dataProvider provideIsEncrypted
+	 */
+	public function test_is_encrypted( string $data, bool $encrypted ): void {
+		$this->assertSame( $encrypted, Crypt::is_encrypted( $data ) );
+	}
+
+
+	public static function provideIsEncrypted(): array {
+		$crypt = Crypt::factory( 'very secret key' );
+		$encrypted = $crypt->encrypt( 'Hello hi how are you? :)' );
+		$array = json_decode( \base64_decode( $encrypted, true ), true );
+		unset( $array['iv'] );
+
+		return [
+			'basic string'         => [ 'Hello hi how are you? :)', false ],
+			'encrypted'            => [ $encrypted, true ],
+			'empty string'         => [ '', false ],
+			'base64'               => [ base64_encode( 'Hello hi how are you? :)' ), false ],
+			'encrypted and string' => [ $encrypted . 'extra', false ],
+			'missing key'          => [ base64_encode( (string) wp_json_encode( $array ) ), false ],
+		];
 	}
 }
