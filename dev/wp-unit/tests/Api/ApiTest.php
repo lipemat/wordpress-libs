d3e68252767341c0db37fe1298d143cd69167dd3
lipemat <mat@matlipe.com>
2025-01-27 11:42:06 -0500

    Fix namespace for ApiTest

--- dev/wp-unit/tests/Api/ApiTest.php
+++ dev/wp-unit/tests/Api/ApiTest.php
@@ -5,9 +5,7 @@
  *
  */
 
-namespace Lipe\Project\Api;
-
-use Lipe\Lib\Api\Api;
+namespace Lipe\Lib\Api;
 
 class ApiTest extends \WP_UnitTestCase {
 	/**
@@ -15,13 +13,14 @@ class ApiTest extends \WP_UnitTestCase {
 	 */
 	private $o;
 
-	public function setUp() : void  {
+
+	public function setUp(): void {
 		parent::setUp();
 		$this->o = Api::in();
 	}
 
 
-	public function test_get_api_url() : void {
+	public function test_get_api_url(): void {
 		$this->assertEquals( $this->o->get_root_url() . 'test/page/4/category/6/?' . Api::FORMAT . '=' . Api::FORMAT_ASSOC, $this->o->get_url( 'test', [
 			'page'     => 4,
 			'category' => 6,
@@ -33,12 +32,11 @@ class ApiTest extends \WP_UnitTestCase {
 			'category',
 			6,
 		] ) );
-
 	}
 
 
-	public function test_handle_request() : void {
-		add_action( $this->o->get_action( __METHOD__ ), function ( $data ) {
+	public function test_handle_request(): void {
+		add_action( $this->o->get_action( __METHOD__ ), function( $data ) {
 			$this->assertSame( [
 				'page'     => '4',
 				'category' => '6',
@@ -55,7 +53,7 @@ class ApiTest extends \WP_UnitTestCase {
 
 		\call_private_method( $this->o, 'handle_request', [ $GLOBALS['wp'] ] );
 
-		add_action( $this->o->get_action( __METHOD__ . 2 ), function ( $data ) {
+		add_action( $this->o->get_action( __METHOD__ . 2 ), function( $data ) {
 			$this->assertSame( [
 				'page',
 				'4',
@@ -65,7 +63,7 @@ class ApiTest extends \WP_UnitTestCase {
 		} );
 
 		unset( $_REQUEST[ Api::FORMAT ] );
-		$GLOBALS['wp']->set_query_var( Api::NAME, str_replace( $this->o->get_root_url(), '', $this->o->get_url( __METHOD__ . 2 , [
+		$GLOBALS['wp']->set_query_var( Api::NAME, str_replace( $this->o->get_root_url(), '', $this->o->get_url( __METHOD__ . 2, [
 			'page',
 			'4',
 			'category',
@@ -73,6 +71,5 @@ class ApiTest extends \WP_UnitTestCase {
 		] ) ) );
 
 		\call_private_method( $this->o, 'handle_request', [ $GLOBALS['wp'] ] );
-
 	}
 }
