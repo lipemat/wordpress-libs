3ce5a5e8c9891a0e4c40ebf5979ff15e1ac4cd8d
lipemat <mat@matlipe.com>
2026-03-04 07:20:59 -0500

    Move deprecated classes out of init and add @deprecated
    Add @deprecated to the classes so PHPStan will pick it up.

--- init.php
+++ init.php
@@ -11,21 +11,12 @@ declare( strict_types=1 );
  * @since  5.6.1
  */
 
-use Lipe\Lib\CMB2\Box\BoxType;
-use Lipe\Lib\CMB2\Field\Default_Callback;
-use Lipe\Lib\CMB2\Field\Display;
-use Lipe\Lib\CMB2\Field\Event_Callbacks;
-use Lipe\Lib\CMB2\Field\Field_Type;
+// Reset the DI container between tests.
 use Lipe\Lib\Container\Container;
 
-// @todo Log to Wiki and remove in version 6.
-\class_alias( BoxType::class, 'Lipe\Lib\CMB2\BoxType' );
-\class_alias( Default_Callback::class, 'Lipe\Lib\CMB2\Default_Callback' );
-\class_alias( Display::class, 'Lipe\Lib\CMB2\Display' );
-\class_alias( Event_Callbacks::class, 'Lipe\Lib\CMB2\Event_Callbacks' );
-\class_alias( Field_Type::class, 'Lipe\Lib\CMB2\Field_Type' );
-
-// Reset the DI container between tests.
 if ( \defined( 'WP_UNIT_DIR' ) ) {
 	add_action( 'wp-unit/reset-container', Container::reset( ... ) );
 }
+
+
+require_once __DIR__ . '/deprecated.php';
