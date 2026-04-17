6a1398dd1ea535b7e924c770ad5b70dc07f5e44b
lipemat <mat@matlipe.com>
2026-04-17 11:44:21 -0400

    Fix spacing of radio and checkbox elements in terms box

--- js/src/gutenberg/meta-boxes/Taxonomy/SimpleTerms.tsx
+++ js/src/gutenberg/meta-boxes/Taxonomy/SimpleTerms.tsx
@@ -1,10 +1,13 @@
 import {CheckboxControl} from '@wordpress/components';
 import type {FromPanel} from './WithTaxonomyPanel';
 
+import styles from './simple.pcss';
+
 const SimpleTerms = ( {assigned, setAssigned, terms, tax}: FromPanel ) => {
 	return ( <>
 		{terms.map( term => ( <div key={term.id}>
 			<CheckboxControl
+				className={styles.control}
 				checked={assigned.includes( term.id )}
 				label={term.name}
 				onChange={checked => {
@@ -14,17 +17,12 @@ const SimpleTerms = ( {assigned, setAssigned, terms, tax}: FromPanel ) => {
 						setAssigned( assigned.filter( id => id !== term.id ) );
 					}
 				}}
+				__nextHasNoMarginBottom
 			/>
 		</div> ) )}
-		<div
-			style={{
-				borderTop: '1px solid #eee',
-				color: '#888',
-				marginTop: '12px',
-				paddingTop: '12px',
-			}}
-		>
+		<div className={styles.none}>
 			<CheckboxControl
+				className={styles.control}
 				checked={0 === assigned.length}
 				label={tax?.labels.no_terms ?? 'None'}
 				onChange={checked => {
@@ -34,6 +32,7 @@ const SimpleTerms = ( {assigned, setAssigned, terms, tax}: FromPanel ) => {
 						setAssigned( terms.map( term => term.id ) );
 					}
 				}}
+				__nextHasNoMarginBottom
 			/>
 		</div>
 	</> );
